b84e96fcf44df9f6357c7d6688f59457f208698a
jzbor <jzbor@posteo.net>
2024-09-19 00:49:30 +0200

    Moving Scraper into a new struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::io;
 use std::net::{TcpListener, TcpStream};
-use std::sync::Mutex;
 use std::time;
 use std::time::Duration;
 use std::time::Instant;
@@ -24,12 +24,6 @@ const BLOCK_CONTENT_FILTER: &str = "Wartende Personen";
 const HTTP_VERSION: &str = "HTTP/1.1";
 /// Time-to-live for [cached](CACHED_FRAME) data frames
 const CACHE_EXPIRATION: Duration = Duration::from_secs(30);
-
-/// Cache the last successful request
-///
-/// The cache expiration behavior is specified by [`CACHE_EXPIRATION`] and is calculated based on
-/// the field [`DataFrame::created_instant`].
-static CACHED_FRAME: Mutex<Option<DataFrame>> = Mutex::new(None);
 
 
 /// Specifies the type of a ticket, which may be either for citizens services, drivers-license
@@ -76,9 +70,19 @@ struct DataFrame {
     created_timestamp: Duration,
 }
 
+/// Carries the state of the scraper
+struct Scraper {
+    /// Cache the last successful request
+    ///
+    /// The cache expiration behavior is specified by [`CACHE_EXPIRATION`] and is calculated based on
+    /// the field [`DataFrame::created_instant`].
+    cache: Option<DataFrame>,
+}
+
 /// Serves queue data over http
 struct Server {
     listener: TcpListener,
+    scraper: RefCell<Scraper>,
 }
 
 /// Http responses
@@ -110,6 +114,7 @@ impl Server {
     pub fn init(addr: &str) -> io::Result<Self> {
         Ok(Server {
             listener: TcpListener::bind(addr)?,
+            scraper: RefCell::new(Scraper::new()),
         })
     }
 
@@ -143,7 +148,7 @@ impl Server {
             let path = request_tokens[1];
 
             if path == "/metrics" {
-                match metrics() {
+                match self.scraper.borrow_mut().metrics() {
                     Ok(response) => Self::send_response(stream, ResponseType::Ok, HashMap::new(), Some(&response)),
                     Err(e) => {
                         eprintln!("Error: {}", e);
@@ -189,6 +194,112 @@ impl Server {
     }
 }
 
+impl Scraper {
+    fn new() -> Self {
+        Scraper {
+            cache: None
+        }
+    }
+
+    /// Create a metrics string in the [Prometheus data format](https://prometheus.io/docs/instrumenting/writing_exporters/).
+    ///
+    /// Metrics are taken either from [cache](CACHED_FRAME) or are [freshly scraped](scrape).
+    fn metrics(&mut self) -> Result<String, String> {
+        use TicketType::*;
+        let data = if self.cache.is_some() && self.cache.as_ref().unwrap().created_instant > Instant::now() - CACHE_EXPIRATION {
+            self.cache.clone().unwrap()
+        } else {
+            let data = Self::scrape()?;
+            self.cache.insert(data.clone())
+                .cached = true;
+            data
+        };
+
+        let mut response = String::new();
+
+        response.push_str("# Information on the citizen service\n");
+        response.push_str(&format!("erth_people_waiting{{service=\"citizen\"}}\t\t{}\n", data.citizen_services.people_waiting));
+        match data.citizen_services.last_called_ticket.0 {
+            B |F => response.push_str(&format!(
+                "erth_last_called_ticket{{service=\"citizen\",type=\"{}\"}}\t{}\n",
+                data.citizen_services.last_called_ticket.0,
+                data.citizen_services.last_called_ticket.1)),
+            None => (),
+        }
+        response.push_str(&format!("erth_waiting_time{{service=\"citizen\"}}\t\t{}\n", data.citizen_services.waiting_time_estimation));
+
+        response.push_str("\n# Information on the drivers-license service\n");
+        response.push_str(&format!("erth_people_waiting{{service=\"drivers_license\"}}\t\t{}\n", data.drivers_license_services.people_waiting));
+        match data.drivers_license_services.last_called_ticket.0 {
+            B |F => response.push_str(&format!(
+                "erth_last_called_ticket{{service=\"drivers_license\",type=\"{}\"}}\t{}\n",
+                data.drivers_license_services.last_called_ticket.0,
+                data.drivers_license_services.last_called_ticket.1)),
+            None => (),
+        }
+        response.push_str(&format!("erth_waiting_time{{service=\"drivers_license\"}}\t\t{}\n", data.drivers_license_services.waiting_time_estimation));
+
+
+        response.push_str("\n# Meta information\n");
+        response.push_str(&format!("erth_cached\t\t{}\n", data.cached as i64));
+        response.push_str(&format!("erth_scrape_duration\t{}\n", data.scrape_duration.as_millis()));
+        response.push_str(&format!("erth_scrape_timestamp\t{}\n", data.created_timestamp.as_millis()));
+
+        Ok(response)
+    }
+
+    /// Scrape new information from the town-hall website
+    fn scrape() -> Result<DataFrame, String> {
+        let start = Instant::now();
+        let response = reqwest::blocking::get(URL)
+            .map_err(|e| e.to_string())?
+            .text()
+            .map_err(|e| e.to_string())?;
+        let document = scraper::Html::parse_document(&response);
+
+        let block_selector = scraper::Selector::parse(BLOCK_SELECTOR)
+            .map_err(|e| e.to_string())?;
+        let line_selector = scraper::Selector::parse(VALUE_SELECTOR)
+            .map_err(|e| e.to_string())?;
+
+        let blocks = document.select(&block_selector)
+            .filter(|b| b.inner_html().contains(BLOCK_CONTENT_FILTER));
+
+        let mut data_frames = Vec::new();
+        for block in blocks {
+            let values: Vec<_> = block.select(&line_selector)
+                .map(|e| e.inner_html())
+                .collect();
+            if values.len() < 3 {
+                return Err(String::from("not enough lines"));
+            }
+
+            let people_waiting = str::parse(&values[0])
+                .map_err(|_| String::from("cannot parse waiting persons"))?;
+            let last_called_ticket = Ticket::parse(&values[1])
+                .map_err(|_| String::from("cannot parse current ticket"))?;
+            let waiting_time_estimation = str::parse(&values[2].strip_suffix(" Minuten").unwrap_or(&values[2]))
+                .map_err(|_| String::from("cannot parse waiting-time estimation"))?;
+
+            data_frames.push(QueueDataFrame { people_waiting, last_called_ticket, waiting_time_estimation });
+        }
+        if data_frames.len() < 2 {
+            return Err(String::from("not enough data blocks"));
+        }
+
+        Ok(DataFrame {
+            citizen_services: data_frames[0].clone(),
+            drivers_license_services: data_frames[1].clone(),
+            scrape_duration: time::Instant::now() - start,
+            cached: false,
+            created_instant: Instant::now(),
+            created_timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap_or(Duration::new(0, 0)),
+        })
+    }
+}
+
 impl Display for TicketType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use TicketType::*;
@@ -201,104 +312,6 @@ impl Display for TicketType {
 }
 
 
-/// Scrape new information from the town-hall website
-fn scrape() -> Result<DataFrame, String> {
-    let start = Instant::now();
-    let response = reqwest::blocking::get(URL)
-        .map_err(|e| e.to_string())?
-        .text()
-        .map_err(|e| e.to_string())?;
-    let document = scraper::Html::parse_document(&response);
-
-    let block_selector = scraper::Selector::parse(BLOCK_SELECTOR)
-        .map_err(|e| e.to_string())?;
-    let line_selector = scraper::Selector::parse(VALUE_SELECTOR)
-        .map_err(|e| e.to_string())?;
-
-    let blocks = document.select(&block_selector)
-        .filter(|b| b.inner_html().contains(BLOCK_CONTENT_FILTER));
-
-    let mut data_frames = Vec::new();
-    for block in blocks {
-        let values: Vec<_> = block.select(&line_selector)
-            .map(|e| e.inner_html())
-            .collect();
-        if values.len() < 3 {
-            return Err(String::from("not enough lines"));
-        }
-
-        let people_waiting = str::parse(&values[0])
-            .map_err(|_| String::from("cannot parse waiting persons"))?;
-        let last_called_ticket = Ticket::parse(&values[1])
-            .map_err(|_| String::from("cannot parse current ticket"))?;
-        let waiting_time_estimation = str::parse(&values[2].strip_suffix(" Minuten").unwrap_or(&values[2]))
-            .map_err(|_| String::from("cannot parse waiting-time estimation"))?;
-
-        data_frames.push(QueueDataFrame { people_waiting, last_called_ticket, waiting_time_estimation });
-    }
-    if data_frames.len() < 2 {
-        return Err(String::from("not enough data blocks"));
-    }
-
-    Ok(DataFrame {
-        citizen_services: data_frames[0].clone(),
-        drivers_license_services: data_frames[1].clone(),
-        scrape_duration: time::Instant::now() - start,
-        cached: false,
-        created_instant: Instant::now(),
-        created_timestamp: SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap_or(Duration::new(0, 0)),
-    })
-}
-
-/// Create a metrics string in the [Prometheus data format](https://prometheus.io/docs/instrumenting/writing_exporters/).
-///
-/// Metrics are taken either from [cache](CACHED_FRAME) or are [freshly scraped](scrape).
-fn metrics() -> Result<String, String> {
-    use TicketType::*;
-    let mut cache = CACHED_FRAME.lock().unwrap();
-    let data = if cache.is_some() && cache.clone().unwrap().created_instant > Instant::now() - CACHE_EXPIRATION {
-        cache.clone().unwrap()
-    } else {
-        let data = scrape()?;
-        cache.insert(data.clone())
-            .cached = true;
-        data
-    };
-
-    let mut response = String::new();
-
-    response.push_str("# Information on the citizen service\n");
-    response.push_str(&format!("erth_people_waiting{{service=\"citizen\"}}\t\t{}\n", data.citizen_services.people_waiting));
-    match data.citizen_services.last_called_ticket.0 {
-        B |F => response.push_str(&format!(
-            "erth_last_called_ticket{{service=\"citizen\",type=\"{}\"}}\t{}\n",
-            data.citizen_services.last_called_ticket.0,
-            data.citizen_services.last_called_ticket.1)),
-        None => (),
-    }
-    response.push_str(&format!("erth_waiting_time{{service=\"citizen\"}}\t\t{}\n", data.citizen_services.waiting_time_estimation));
-
-    response.push_str("\n# Information on the drivers-license service\n");
-    response.push_str(&format!("erth_people_waiting{{service=\"drivers_license\"}}\t\t{}\n", data.drivers_license_services.people_waiting));
-    match data.drivers_license_services.last_called_ticket.0 {
-        B |F => response.push_str(&format!(
-            "erth_last_called_ticket{{service=\"drivers_license\",type=\"{}\"}}\t{}\n",
-            data.drivers_license_services.last_called_ticket.0,
-            data.drivers_license_services.last_called_ticket.1)),
-        None => (),
-    }
-    response.push_str(&format!("erth_waiting_time{{service=\"drivers_license\"}}\t\t{}\n", data.drivers_license_services.waiting_time_estimation));
-
-
-    response.push_str("\n# Meta information\n");
-    response.push_str(&format!("erth_cached\t\t{}\n", data.cached as i64));
-    response.push_str(&format!("erth_scrape_duration\t{}\n", data.scrape_duration.as_millis()));
-    response.push_str(&format!("erth_scrape_timestamp\t{}\n", data.created_timestamp.as_millis()));
-
-    Ok(response)
-}
 
 fn main() {
     let mut server = Server::init("localhost:12080").unwrap();
